53ced358c9be0425f477561169c56fedb08a73f5
JotaaGamer Gameplays <jorluos@javerianacali.edu.co>
2023-11-14 23:10:09 -0500

    FUNCION 2

---
+++ Programa.py
@@ -0,0 +1,6 @@
+def preguntarCharacter():
+    character = input(" POR FAVOR INGRESAR EL VALOR A CAMBIAR \n :")
+    return character
+def preguntarPalabra():
+    palabra = input(" POR FAVOR INGRESA LA PALABRA QUE DESEAS CAMBIAR \n :")
+    return palabra
